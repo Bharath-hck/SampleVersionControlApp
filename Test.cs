49ff949860ec5a44fe74480a7f3e4a9b4540e7b1
Bharath <bharath@hacktivate.in>
2024-04-09 09:58:04 +0530

    Added test file

---
+++ Test.cs
@@ -0,0 +1,10 @@
+namespace SampleVersionControlApp;
+class Test
+{
+    static void TestMethod(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Adding a second line");
+        Console.WriteLine("Added from dev branch");
+    }
+}
