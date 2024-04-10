d5ad75a64e4a9e6b125078f7c6ca54bda00261e1
Bharath <bharath@hacktivate.in>
2024-04-10 09:54:12 +0530

    Added new change to test remote push

--- Program.cs
+++ Program.cs
@@ -6,5 +6,6 @@ class Program
         Console.WriteLine("Hello, World!");
         Console.WriteLine("Adding a second line");
         Console.WriteLine("Added from dev branch");
+        Console.WriteLine("Added new change to push to remote");
     }
 }
